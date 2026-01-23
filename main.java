bb539185d004c016ea18ef9fa8e465319dcad2be
vijayapraksh24 <vijayaprakash.m.v.s@gmail.com>
2026-01-23 06:47:11 +0530

    commit

---
+++ main.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+
+public class main {
+
+    static void submain(){
+        Scanner set=new Scanner(System.in);
+        System.out.print("Enter the value for x:");
+       int x=set.nextInt();
+      switch(x){
+        case 1:
+            System.out.println("It's 1");
+            break;
+        case 2:
+            System.out.println("It's 2");
+            break;
+        case 3:
+            System.out.println("It's 3");
+            break;
+        case 4:
+            System.out.println("It's 4");
+            break;
+        case 5:
+            System.out.println("It's 5");
+            break;
+        default:
+            System.out.println("it is not in the list");
+       }
+    }
+    public static void main(String[] args){
+        // Scanner set=new Scanner(System.in);
+        // System.out.print("Enter the Value to check whether the number is greater the 10 or not:");
+        // int con=set.nextInt();
+       
+        // if(con==10){
+        //     System.out.println("Given Number is Equal to 10");
+        //     System.out.print("Enter next Number:");
+        //     int next=set.nextInt();
+        //     if(next==11){
+        //         System.out.println("Next Number is correct");
+        //     }else{
+        //         System.out.println("Next number is  11.");
+        //     }
+
+        // }else if(con>10){
+        //     System.out.println("Given Number is Greater than 10");
+        // }
+        // else{
+        //     System.out.println("Given number is Less than 10");
+        // }
+        // submain();
+
+        // int a=10;
+        // System.out.println(a++ + " " + ++a + " " + a);
+        // System.out.println(++a + " " + a + " " + a++);
+        // System.out.println(a++ + " " + a + " " + a++);
+        // Boolean b=3<5;
+        
+        // System.out.println(b);
+        // System.out.println(5<4 && 4>5);
+        // System.out.println(5>4 & 4>5);
+        // int x,y,z;
+        // x=20;
+        // y=10;
+        // z=(x>y)?x:y;
+        // System.out.println(z);
+
+        // System.out.println(b==true?true:false);
+
+        //  int a=10;
+        //  System.out.println((a%2==0)?("its even"):("its odd"));
+
+        //  int a,b,c;
+        //  a=10;b=20;c=404;
+        //  System.out.println((a>b)?((a>c)?a:c):((b>c)?b:c));
+
+        // int a=0;
+        
+        // System.out.println((a>=1)?"positive":((a<0)?"negative":"its zero"));
+
+        // int age=17;
+        // System.out.println((age>=18)?"You are major":"You are minor");
+
+        // int nu=101;
+        // System.out.println((nu>=1000 && nu<=9999)?"it is four digit number":"it is not four digit number");
+         
+        // int a=423,b=526;
+        // System.out.println((a&b) +" "+ (a|b)+ " "+(a^b));
+
+        // System.out.println("eample:"+(3+2)+5);
+        // System.out.println('A'+'B'+"hai");
+        // System.out.println('A'+"B"+"hai");
+        // System.out.println('A'+'B'+'C'+'D');
+        // System.out.println('A'+10+'B'+"hai");
+
+
+        // float x=2*5/3;
+        // System.out.println(x);
+        // System.out.println(10 + +10 * 2);
+
+        // int a=2593;
+        // System.out.println(a<<2);
+        // System.out.println(a>>6);
+
+        // Scanner in=new Scanner(System.in);
+        // System.out.print("Enter radius:");
+        // double radius=in.nextDouble();
+        // double pi=3.14;
+        // double area;
+        // area=pi*(radius*radius);
+        // System.out.println("Area of Circle="+" "+area);
+
+        // System.out.print("Enter celcius:");
+        // float celcius=in.nextFloat();
+        // float farenheit=(celcius*9/5)+32;
+        // System.out.println("Farenheit="+" "+farenheit);
+
+        // System.out.print("Enter A B C:");
+        // float a=in.nextFloat();
+        // float b=in.nextFloat();
+        // float c=in.nextFloat();
+        // float ans=a+b+c;
+        // System.out.println("Perimeter of Triangel="+" "+ans);
+
+        // System.out.print("Enter Farenheit:");
+        // float farenheit1=in.nextFloat();
+        // float celcius1=(farenheit1-32)*5/9;
+        // System.out.println("Celcius="+" "+celcius1);
+
+        System.out.println(0xA+0xB);
+        
+
+
+
+
+    }
+}
