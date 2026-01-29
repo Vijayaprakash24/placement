7d211903fe7f87a010c77a9a0dd7f6cc940cc577
vijayapraksh24 <vijayaprakash.m.v.s@gmail.com>
2026-01-29 16:26:10 +0530

    commit

---
+++ day5.java
@@ -0,0 +1,209 @@
+public class day5 {
+    public static void main(String[] args) {
+        // String hex=" ";
+        // int dec=173;
+        // while(dec>0){
+        //     int rem=dec%16;
+        //     if(rem<0){
+        //           hex=rem+hex;
+        //     }else if(rem>=10){
+        //         hex=(char)((rem-10)+'A')+hex;
+        //     }
+        //     dec/=16;
+        // }
+        // System.out.println(hex);
+
+        // String binary=" ";
+        // int dec=173;
+        // while(dec>0){
+        //     int rem=dec%2;
+        //     if(rem>=0){
+        //           binary=rem+binary;
+        //     }
+        //     dec/=2;
+        // }
+        // System.out.println(binary);
+
+        // String octal=" ";
+        // int dec1=173;
+        // while(dec1>0){
+        //     int rem=dec1%8;
+        //     if(rem>=0){
+        //           octal=rem+octal;
+        //     }
+        //     dec1/=8;
+        // }
+        // System.out.println(octal);
+
+        // int value;
+        // String octal=" ";
+        // String binary="1010111";
+        // while(binary.length()%3!=0){
+        //     binary="0"+binary;
+        // } 
+        // for(int i=0;i<binary.length();i=i+3){
+        //    value=0;
+        //    value+=(binary.charAt(i)-'0')*4;
+        //    value+=(binary.charAt(i+1)-'0')*2;
+        //    value+=(binary.charAt(i+2)-'0')*1;
+        //    octal+=value;
+
+        // }
+        // System.out.println(octal);
+
+        // int value;
+        // String octal=" ";
+        // String binary="11111";
+        // while(binary.length()%4!=0){
+        //     binary="0"+binary;
+        // } 
+        // for(int i=0;i<binary.length();i=i+4){
+        //    value=0;
+        //    value+=(binary.charAt(i)-'0')*8;
+        //    value+=(binary.charAt(i+1)-'0')*4;
+        //    value+=(binary.charAt(i+2)-'0')*2;
+        //    value+=(binary.charAt(i+3)-'0')*1;
+        //    if(value>10){
+        //     int rem=value%16;
+        //     octal+=(char)((rem-10)+'A');
+        //    }else{
+        //    octal+=value;
+        // }
+
+        // }
+        // System.out.println(octal);
+
+        // String hex="1F";
+        // String bin=" ";
+        // for(int i=0;i<hex.length();i++){
+        //     if(hex.charAt(i)>='0' && hex.charAt(i)<='9'){
+        //         int dec=hex.charAt(i)-'0';
+                
+               
+        //         while(dec>0){
+
+        //             int rem=dec%2;
+        //             if(rem>=0){
+        //             bin=rem+bin;
+        //             }
+
+               
+        //         dec/=2;
+        //          }
+        //     }else{
+        //         int dec=((hex.charAt(i)-'A')+10);
+        //          while(dec>0){
+
+        //             int rem=dec%2;
+        //             if(rem>=0){
+        //             bin=rem+bin;
+        //             }
+
+               
+        //         dec/=2;
+        //          }
+        //     }
+            
+        // }
+        // System.out.println(bin);
+        
+        // String hex="1F";
+        // String bin=" ";
+        // for(int i=0;i<hex.length();i++){
+        //     if(hex.charAt(i)>='0' && hex.charAt(i)<='7'){
+        //         int dec=hex.charAt(i)-'0';
+                
+               
+        //         while(dec>0){
+
+        //             int rem=dec%8;
+        //             if(rem>=0){
+        //             bin=rem+bin;
+        //             }
+
+               
+        //         dec/=8;
+        //          }
+        //     }else{
+        //         int dec=((hex.charAt(i)-'A')+10);
+        //         System.out.println(dec);
+        //          while(dec>0){
+
+        //             int rem=dec%8;
+        //             if(rem>=0){
+        //             bin=rem+bin;
+        //             }
+
+               
+        //         dec/=8;
+        //          }
+        //     }
+            
+        // }
+        // System.out.println(bin);
+
+        // String n1="010011";
+        // String n2="101010";
+        // int sum;
+        // int carry=0;
+        // String sum1="";
+    
+        // for(int i=n1.length()-1;i>=0;i--){
+        //     int n3=n1.charAt(i)-'0';
+        //     int n4=n2.charAt(i)-'0';
+        //     n3=n3%10;
+        //     n4=n4%10;
+          
+        //     if(n3==1&& n4==1){
+        //         if(carry!=0){
+        //             sum=0;
+        //             carry=1;
+        //         }else{
+        //             sum=0;
+        //             carry=1;
+        //         }
+        //     }else if(n3==0 && n4==1){
+            
+        //         if(carry!=0){
+        //             sum=0;
+        //             carry=1;
+        //         }else{
+        //             sum=1;
+        //         }
+        //     }else if(n3==1&& n4==0){
+        //         if(carry!=0){
+        //             sum=0;
+        //             carry=1;
+        //         }else{
+        //             sum=1;
+        //         }
+        //     }else{
+        //         if(carry!=0){
+        //             sum=1;
+        //             carry=0;
+        //         }else{
+        //             sum=0;
+        //             carry=0;
+        //         }
+                    
+        //         }
+        //         n3/=10;
+        //         n4/=10;
+              
+        //       sum1=String.valueOf(sum)+sum1;
+              
+                
+        // }
+        // System.out.print(carry);
+        // System.out.print(sum1);
+
+        String a="73";
+        String b="64";
+        int a1=a.charAt(0)-'0';
+        int a2=b.charAt(0)-'0';
+        int d=(a1+a2)%8;
+        System.out.print(da);
+       
+
+    }
+}
---
+++ day6.java
@@ -0,0 +1,203 @@
+import java.util.*;
+
+public class day6 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        // int n;
+        // // int[] a=new int[5];
+        // int[] a={10,2,3,4,5};
+        // System.out.println("Enter number upto 5:");
+        // n=in.nextInt();
+        // // for(int i=0;i<n;i++){
+        // //     a[i]=in.nextInt();
+        // // }
+        // for(int i=0;i<a.length;i++){
+        //     System.out.println(a[i]);
+        // }
+
+        // int[] a={1,3,10,-4,1};
+        // int min=a[0];
+        // for(int i=1;i<a.length;i++){
+        //     if(a[i]<min){
+        //         min=a[i];
+        //     }
+        // }
+        // System.out.print(min);
+
+        // int[] a={1,3,10,-4,1};
+        // int max=a[0];
+        // for(int i=1;i<a.length;i++){
+        //     if(a[i]>max){
+        //         max=a[i];
+        //     }
+        // }
+        // System.out.print(max);
+
+        // int[] a={10,3,4,5,-9,6};
+        // int min=a[0];
+        // int secondmin=0;
+
+        // for(int i=1;i<a.length;i++){
+        //     if(a[i]<min){
+        //         secondmin=min;
+        //         min=a[i];
+                
+        //     }else if (a[i] < secondmin && a[i] > secondmin) {
+        //         secondmin = a[i];
+        //     }
+        // }
+        // System.out.println(secondmin);
+
+    
+        // int max=a[0];
+        // int secondmax=0;
+
+        // for(int i=1;i<a.length;i++){
+        //     if(a[i]>max){
+        //         secondmax=max;
+        //         max=a[i];
+                
+        //     }else if (a[i] > secondmax && a[i] < max) {
+        //         secondmax = a[i];
+        //     }
+        // }
+        // System.out.println(secondmax);
+
+        // int[] a={2,1,4,5,7,6};
+        // for(int i=0;i<a.length-1;i++){
+        //     for(int j=i+1;j<a.length;j++){
+        //         if(a[i]>a[j]){
+        //             int temp=a[i];
+        //             a[i]=a[j];
+        //             a[j]=temp;
+        //         }
+        //     }
+        // }for(int i=0;i<a.length;i++){
+        //     System.out.println(a[i]);
+        // }
+        // System.out.println("");
+
+       
+        // for(int i=0;i<a.length-1;i++){
+        //     for(int j=i+1;j<a.length;j++){
+        //         if(a[i]<a[j]){
+        //             int temp=a[i];
+        //             a[i]=a[j];
+        //             a[j]=temp;
+        //         }
+        //     }
+        // }for(int i=0;i<a.length;i++){
+        //     System.out.println(a[i]);
+        // }
+
+        // int[] a={1,2,4,5,6,7,8};
+        // for(int i=0;i<a.length-3;i++){
+        //     for(int j=a.length-(i+1);j==a.length-(i+1);j++){
+              
+        //         int temp=a[i];
+        //         a[i]=a[j];
+        //         a[j]=temp;
+        //     }
+        // }
+        // for(int i=0;i<a.length;i++){
+        //     System.out.println(a[i]);
+        // }
+
+        // int i,j;
+        // int[] b=new int[a.length];
+        // for(i=a.length-1,j=0;i>=0;i--,j++){
+        //     b[i]=a[j];
+        // }
+        // for( i=0;i<a.length;i++){
+        //     System.out.println(b[i]);
+        // }
+
+        //duplicate element in array
+        //    int[] a = {1,2,5,1,2,1,5,3,3,4};
+        //     int count = 0;
+           
+        //     for (int i = 0; i < a.length; i++) {
+        //          Boolean alreadycount=false;
+        //         for(int k=0;k<i;k++){
+        //             if(a[i]==a[k]){
+        //             alreadycount=true;
+        //             break;}
+        //         }
+        //         if(alreadycount){
+        //              continue;
+        //             }
+        //         for (int j = i + 1; j < a.length; j++) {
+        //             if (a[i] == a[j]) {
+        //                 count++;
+        //                 break;   
+        //             }
+        //         }
+        //     }
+
+        //     System.out.println(count);
+
+         
+
+        // for(int i=0;i<a.length;i++){
+        //     Boolean unique=true;
+        //     for(int j=0;j<a.length;j++){
+        //         if(i!=j && a[j]==a[i]){
+        //             unique=false;
+        //             break;
+        //         }
+        //     }
+        //         if(unique){
+        //             System.out.print(a[i]);
+        //         }
+            
+        // }
+    //   int[] a = {1,2,3,1,2,2,4,4,5};
+    //     boolean[] v=new boolean[a.length];
+    //     int c2=0;
+    //     int un=0;
+    //     for(int i=0;i<a.length;i++){
+    //         if(v[i]==true){
+    //             continue;
+    //         }
+    //         int count1=0;
+    //         for(int j=i+1;j<a.length;j++){
+    //             if(a[i]==a[j]){
+    //                 count1+=1;
+    //                 v[j]=true;
+    //             }
+    //         }
+    //         if(count1>=2){
+    //             count1=1;
+    //         }
+    //         if(count1==0){
+    //             System.out.println(a[i]);
+    //             un++;
+    //         }
+    //         c2=count1+c2;
+    //     }
+    //     System.out.println(un);
+    
+   
+    int[] a={1,2,3,4};
+    int[] b={5,6,7};
+     int[] c1=new int[a.length+b.length];
+    int c=a.length+b.length;
+    int j=0;
+    for(int i=0;i<a.length;i++){
+       c1[i]=a[i];
+    }
+  
+     for(int i=0;i<b.length;i++){
+       c1[a.length+i]=b[i];
+      
+    }
+    for(int i=0;i<c;i++){
+            System.out.println(c1[i]);
+        }
+    
+
+
+
+
+    }
+}
